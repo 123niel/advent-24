b9f9b4efea0b3b05e86a89684dd929e1d0b9d3dc
Niel Rohling <niel_samuel.rohling@mail.schwarz>
2024-12-02 15:27:59 +0100

    Day 1+2

---
+++ build.gradle.kts
@@ -0,0 +1,14 @@
+plugins {
+    kotlin("jvm") version "2.1.0"
+}
+
+group = "de.niel123"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("com.toldoven.aoc:aoc-kotlin-notebook:1.0.0")
+}
